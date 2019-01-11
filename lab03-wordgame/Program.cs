7c67931c386aeb884e048fa17daf99379d516d91
juliely8619 <july8619@yahoo.com>
2019-01-10 21:32:04 -0800

    added admin menu

--- lab03-wordgame/Program.cs
+++ lab03-wordgame/Program.cs
@@ -84,6 +84,7 @@ namespace lab03_wordgame
                             break;
                         case 2: //Admin
                             Console.WriteLine("just for testing, in option 2");
+                            AdminMenuSelection();
                             break;
                         default: // exit
                             Console.WriteLine("just for testing, in option 3");
@@ -107,14 +108,73 @@ namespace lab03_wordgame
             {
                 Console.WriteLine("Something went wrong");
             }
-
-
-            //public static int Test(int num)
-            //{
-            //    return num;
-            //}
-
-
         }
+
+        //show admin menu
+        public static void AdminMenuSelection()
+        {
+            Console.WriteLine("Make a selection");
+            Console.WriteLine("1: View word list");
+            Console.WriteLine("2: Add a word");
+            Console.WriteLine("3: Delete a word");
+            Console.WriteLine("4: Exit Admin");
+            try
+            {
+                string userChoice = Console.ReadLine();
+                int userChoiceInt = Convert.ToInt32(userChoice);
+
+                if (userChoiceInt > 4)
+                {
+                    Console.WriteLine("That isn't an option");
+                    AdminMenuSelection();
+                }
+                else if (userChoiceInt < 0)
+                {
+                    Console.WriteLine("That isn't an option");
+                    AdminMenuSelection();
+                }
+                else //couldnt do while loop because I could enter bad code on first entry
+                {
+                    switch (userChoiceInt)
+                    {
+                        case 1: //view list
+                            Console.WriteLine("just for testing, in option 1");
+                            break;
+                        case 2: //add a word
+                            Console.WriteLine("just for testing, in option 2");
+                            break;
+                        case 3: //delete  word
+                            Console.WriteLine("just for testing, in option 3");
+                            break;
+                        default: // exit
+                            Console.WriteLine("just for testing, in option 4");
+                            Console.ReadLine();
+                            MainMenuSelection();
+                            //Environment.Exit(0);
+                            break;
+                    }
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("You didn't make an entry. Try again.");
+                AdminMenuSelection();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("There was an error reading your input, please try again.");
+                AdminMenuSelection();
+            }
+            catch (Exception) //general
+            {
+                Console.WriteLine("Something went wrong");
+            }
+        }
+
+
+                //public static int Test(int num)
+                //{
+                //    return num;
+                //}
     }
 }
