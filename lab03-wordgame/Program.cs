8ceb568999e65be669868ff1c31dfd589fe0f246
juliely8619 <july8619@yahoo.com>
2019-01-10 20:18:00 -0800

    have read file, and append file

--- lab03-wordgame/Program.cs
+++ lab03-wordgame/Program.cs
@@ -9,16 +9,41 @@ namespace lab03_wordgame
         {
             string path = "../../../testFile.txt";
             CreateFile(path);
+            ReadFile(path);
+            AppendToFile(path);
+            ReadFile(path);
+
+            Console.ReadLine(); //so prog doesn't auto quit
         }
 
         private static void CreateFile(string path)
         {
             using (StreamWriter streamWriter = new StreamWriter(path))
             {
-                streamWriter.WriteLine("Made it into the test file");
+                streamWriter.WriteLine("Word 1");
+                streamWriter.WriteLine("word 2");
+                streamWriter.WriteLine("Word 3");
+                streamWriter.WriteLine("tra la la");
             }
         }
 
+        private static void ReadFile(string path)
+        {
+            string[] linesFromFile = File.ReadAllLines(path);
+            for (int i = 0; i < linesFromFile.Length; i++)
+            {
+                Console.WriteLine(linesFromFile[i]);
+            }
+        }
+
+        private static void AppendToFile(string path)
+        {
+            using (StreamWriter streamWriter = File.AppendText(path))
+            {
+                streamWriter.WriteLine("NEW WORD");
+            }
+
+        }
         //public static int Test(int num)
         //{
         //    return num;
