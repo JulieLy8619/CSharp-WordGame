1e9155e0ce2e01686959c8e732877f6bb97f6465
juliely8619 <july8619@yahoo.com>
2019-01-10 21:24:37 -0800

    have menu

--- lab03-wordgame/Program.cs
+++ lab03-wordgame/Program.cs
@@ -7,23 +7,29 @@ namespace lab03_wordgame
     {
         static void Main(string[] args)
         {
-            string path = "../../../testFile.txt";
-            CreateFile(path);
-            ReadFile(path);
-            AppendToFile(path);
-            ReadFile(path);
-
+            string path = "../../../WordFile.txt";
+            MainMenuSelection();
+            //CreateFile(path); //create the file when they choose to play a game
+            //ReadFile(path); // when in admin and want to see the word list
             Console.ReadLine(); //so prog doesn't auto quit
         }
 
+        //sets up default word list
         private static void CreateFile(string path)
         {
             using (StreamWriter streamWriter = new StreamWriter(path))
             {
-                streamWriter.WriteLine("Word 1");
-                streamWriter.WriteLine("word 2");
-                streamWriter.WriteLine("Word 3");
-                streamWriter.WriteLine("tra la la");
+                //defaul word list
+                streamWriter.WriteLine("SEATTLE");
+                streamWriter.WriteLine("BAT");
+                streamWriter.WriteLine("WASHINGTON");
+                streamWriter.WriteLine("FIDDLE");
+                streamWriter.WriteLine("SQUASH");
+                streamWriter.WriteLine("MISSISSIPPI");
+                streamWriter.WriteLine("SCIENTIST");
+                streamWriter.WriteLine("TEN");
+                streamWriter.WriteLine("CLASS");
+                streamWriter.WriteLine("STUDENT");
             }
         }
 
@@ -40,19 +46,75 @@ namespace lab03_wordgame
         {
             using (StreamWriter streamWriter = File.AppendText(path))
             {
-                streamWriter.WriteLine("NEW WORD");
+                Console.WriteLine("What word would you like to add to the list?");
+                string userChoice = Console.ReadLine();
+                string userChoiceCap = userChoice.ToUpper();
+                streamWriter.WriteLine(userChoiceCap);
+            }
+        }
+
+        //show main menu
+        public static void MainMenuSelection()
+        {
+            Console.WriteLine("Make a selection");
+            Console.WriteLine("1: Start a new game");
+            Console.WriteLine("2: Admin");
+            Console.WriteLine("3: Exit");
+            try
+            {
+                string userChoice = Console.ReadLine();
+                int userChoiceInt = Convert.ToInt32(userChoice);
+
+                if (userChoiceInt > 4)
+                {
+                    Console.WriteLine("That isn't an option");
+                    MainMenuSelection();
+                }
+                else if (userChoiceInt < 0)
+                {
+                    Console.WriteLine("That isn't an option");
+                    MainMenuSelection();
+                }
+                else //couldnt do while loop because I could enter bad code on first entry
+                {
+                    switch (userChoiceInt)
+                    {
+                        case 1: //Start
+                            Console.WriteLine("just for testing, in option 1");
+                            break;
+                        case 2: //Admin
+                            Console.WriteLine("just for testing, in option 2");
+                            break;
+                        default: // exit
+                            Console.WriteLine("just for testing, in option 3");
+                            Console.ReadLine();
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("You didn't make an entry. Try again.");
+                MainMenuSelection();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("There was an error reading your input, please try again.");
+                MainMenuSelection();
+            }
+            catch (Exception) //general
+            {
+                Console.WriteLine("Something went wrong");
             }
 
+
+            //public static int Test(int num)
+            //{
+            //    return num;
+            //}
+
+
         }
-        //public static int Test(int num)
-        //{
-        //    return num;
-        //}
-
-        //test read a file
-
-        //test write a file
-
-
     }
 }
